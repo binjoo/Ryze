44525b6eb485a94de2fa4eef59ca2c1636df89ae
binjoo <icesword28@qq.com>
2016-04-10 00:32:16 +0800

    重新规划页面js编写

--- web/assets/js/admin.js
+++ web/assets/js/admin.js
@@ -1,9 +1,12 @@
 var pageObj = window.NameSpace || {};
-pageObj.randomAESKey = function(){
-	$("input[name=wechat_aeskey]").val(Utils.randomWord(false, 43));
-}
-
+/**
+ * 后台首页
+ */
 pageObj.index = new function() {
+	this.init = function(){
+		$("ul.navbar-menu a").click(this.header_click);
+		$("ul.nav-sidebar a").click(this.sidebar_click);
+	},
 	this.header_click = function(){
 		$("ul.navbar-menu a").parent().removeClass();
 		$(this).parent().addClass("active");
@@ -15,13 +18,37 @@ pageObj.index = new function() {
 	this.sidebar_click = function(){
 		$("ul.nav-sidebar a").parent().removeClass();
 		$(this).parent().addClass("active");
-	}
+	};
 };
 
+/**
+ * 论坛-节点管理
+ */
+pageObj.node = new function(){
+	this.init = function(){
+		$("#node_save").click(this.btn_save);
+	},
+	this.btn_search = function(){
+		
+	},
+	this.btn_save = function(){
+		var con = false;
+		$(".modal-body input").parent().removeClass("has-error");
+		if($("input[name=name]").val() == ""){
+			con = true;
+			$("input[name=name]").parent().addClass("has-error");
+		}
+		if($("input[name=no]").val() == ""){
+			con = true;
+			$("input[name=no]").parent().addClass("has-error");
+		}
+		if(con) return;
+		$("form[name=node_edit]").submit();
+	}
+}
+
 $(document).ready(function() {
-	//微信随机生成EncodingAESKey
-	$("#randomAESKey").click(pageObj.randomAESKey);
-	$("ul.navbar-menu a").click(pageObj.index.header_click);
-	$("ul.nav-sidebar a").click(pageObj.index.sidebar_click);
-	pageObj.index.sidebar_click;
+	for (obj in pageObj) {
+    	pageObj[obj].init();
+	}
 });
